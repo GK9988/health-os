fc36faa695709ad6e940b8d662c93b648638f10d
Garv Khurana <gk12345garvkhurana@gmail.com>
2026-03-06 03:29:30 +0530

    HOS-001-1: Service Inits Initialized: 1. Supplement Service 2. Ai Service

---
+++ supplement-service/src/test/java/com/garvk/supplementservice/SupplementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garvk.supplementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
